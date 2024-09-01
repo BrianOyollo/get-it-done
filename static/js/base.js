e1263316f7366d4657b50e079753b2f0893e9f3e
oyollobrian <oyollobrian@gmail.com>
2024-09-01 12:08:02 +0300

    Reinitialize Magnific Popup after HTMX content swaps

--- static/js/base.js
+++ static/js/base.js
@@ -1,23 +1,25 @@
-$(document).ready(function(){
-    $('.image-popup-vertical-fit').magnificPopup({
-        type: 'image',
-      mainClass: 'mfp-with-zoom', 
-      gallery:{
-                enabled:true
-            },
-    
-      zoom: {
-        enabled: true, 
-    
-        duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
-    
-        opener: function(openerElement) {
-    
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
+function initializeMagnificPopup(){
+  $('.image-popup-vertical-fit').magnificPopup({
+    type: 'image',
+    mainClass: 'mfp-with-zoom', 
+    gallery:{ enabled:true },
+
+    zoom: {
+      enabled: true, 
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function(openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
       }
     }
-    
-    });
-    
-    });
+
+  });
+}
+
+$(document).ready(function(){
+  initializeMagnificPopup();
+});
+
+document.addEventListener('htmx:afterSwap', function(event) {
+  initializeMagnificPopup();
+});
